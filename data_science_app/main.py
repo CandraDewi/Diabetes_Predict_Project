85a8d24ac2686d2a357823a1a87b8ea9134bfd9e
Vania Noverina <160705515+vaniavee@users.noreply.github.com>
2025-05-13 15:46:05 +0700

    updateee main.py

--- data_science_app/main.py
+++ data_science_app/main.py
@@ -1,7 +1,10 @@
-import tkinter as tk
-from ui.app_ui import DataScienceApp
+import tkinter as tk # Untuk membuat GUI
+from ui.app_ui import DataScienceApp # mengimpor kelas DataScienceApp dari modul app_ui yang ada di package ui.
 
 if __name__ == "__main__":
-    root = tk.Tk()
+    # Root adalah variabel yang digunakan sebagai referensi untuk primary window.
+    root = tk.Tk() # Membuat window utama aplikasi
+    # DataScienceApp akan membangun UI di dalam root (primary window).
     app = DataScienceApp(root)
+    # Aplikasi akan terus berjalan dan merespon input user sampai window ditutup.
     root.mainloop()
