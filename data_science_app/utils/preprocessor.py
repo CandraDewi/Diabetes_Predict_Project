0f917829af8cbd8c056c947d93cea9653bde39d2
Habibah Ratna Fadhila IH <161398011+dhila0129@users.noreply.github.com>
2025-05-13 15:43:32 +0700

    update comment.py

--- data_science_app/utils/preprocessor.py
+++ data_science_app/utils/preprocessor.py
@@ -1,12 +1,14 @@
-#Data preprocessing
+# Fungsi utama untuk melakukan preprocessing data
 def preprocess_data(df):
-    #Menhapus kolom outcome jika terdapat kolom outcome
+    # Mengecek apakah kolom 'Outcome' ada di dalam data
+    # Kalau ada, kolom 'Outcome' akan dihapus karena dianggap sebagai label (target)
     if 'Outcome' in df.columns:
         df = df.drop("Outcome", axis=1)
 
-    #Jika ada missing value, maka akan diganti dengan nilai median
+    # Jika ada missing value, maka akan diganti dengan nilai median
     columns = ["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]
     for col in columns:
         df[col] = df[col].replace(0, df[col].median())
     
+    # Setelah semua proses selesai, dataframe yang sudah diproses akan dikembalikan
     return df
