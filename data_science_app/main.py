854590abf49efec3063a07ef10b34576d1ef1d7c
CandraDewi <87966244+CandraDewi@users.noreply.github.com>
2025-05-14 14:47:38 +0700

    update another revesion

---
+++ data_science_app/main.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from ui.app_ui import DataScienceApp
+
+# Menjalankan class DataScienceApp sebagai main app
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = DataScienceApp(root)
+    root.mainloop()
