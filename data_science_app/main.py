ed2d82b5b7ac1c9ae51e13786333308e8ea0614b
CandraDewi <87966244+CandraDewi@users.noreply.github.com>
2025-06-11 01:55:17 +0700

    Delete data_science_app directory

--- data_science_app/main.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from ui.app_ui import DataScienceApp
-
-# Menjalankan class DataScienceApp sebagai main app
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = DataScienceApp(root)
-    root.mainloop()
